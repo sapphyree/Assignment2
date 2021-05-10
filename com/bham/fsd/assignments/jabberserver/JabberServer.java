576a350ba269497df0187ba2bf9754d3902f3f00
sapphicaa <sonexix@gmail.com>
2021-05-10 12:33:27 +0100

    constructor implementation

--- com/bham/fsd/assignments/jabberserver/JabberServer.java
+++ com/bham/fsd/assignments/jabberserver/JabberServer.java
@@ -1,4 +1,31 @@
 package com.bham.fsd.assignments.jabberserver;
+
+import java.io.*;
+import java.net.*;
+
 public class JabberServer {
     
+    private static final int PORT_NUMBER = 44444;
+    private ServerSocket serverSocket;
+
+
+    public JabberServer() {
+        try{
+            ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+            System.out.println("Server online.");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String arg[])
+    {
+        new JabberServer();
+    }
+
+
 }
